dbaf91cb7400fd0350d9bb289147d92fffba46a0
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-05 23:48:10 +0100

    CommonRepository Implementation added for common methods, that are shared across all Repositories  - uses injected EntityManager (MongoOperations) to extend Repository functionality

---
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/CommonRepositoryImpl.java
@@ -0,0 +1,33 @@
+package at.fhv.sportsclub.repository;
+
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.repository.query.MongoEntityInformation;
+import org.springframework.data.mongodb.repository.support.SimpleMongoRepository;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+public class CommonRepositoryImpl<T, ID extends Serializable>
+        extends SimpleMongoRepository<T, ID> implements CommonRepository<T,ID> {
+
+    private MongoEntityInformation<T, ID> entityMetaData;
+    private MongoOperations mongoOperations;
+
+    public CommonRepositoryImpl(MongoEntityInformation<T, ID> metadata, MongoOperations mongoOperations) {
+        super(metadata, mongoOperations);
+        this.mongoOperations = mongoOperations;
+        this.entityMetaData = metadata;
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return Optional.ofNullable(
+                mongoOperations.findById(id, entityMetaData.getJavaType())
+        );
+    }
+
+    @Override
+    public <S extends T> S saveOrUpdate(S entity) {
+        return null;
+    }
+}

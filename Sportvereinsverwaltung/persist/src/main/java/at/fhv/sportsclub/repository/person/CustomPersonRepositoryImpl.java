af83cfc61f309caba23c8233b6ebc8d77c4d189d
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-05 23:45:33 +0100

    Interface and Impl Class added for customized request on the PersonRepository

---
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/person/CustomPersonRepositoryImpl.java
@@ -0,0 +1,7 @@
+package at.fhv.sportsclub.repository.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+public class CustomPersonRepositoryImpl implements CustomPersonRepository {
+}

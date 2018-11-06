93df577fe173db1cfe0b961cb3b4be4d6aa841bb
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-06 14:23:55 +0100

    saveOrUpdate method implemented

--- Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/person/CustomPersonRepositoryImpl.java
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/person/CustomPersonRepositoryImpl.java
@@ -1,7 +1,20 @@
 package at.fhv.sportsclub.repository.person;
 
+import at.fhv.sportsclub.entity.person.PersonEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CustomPersonRepositoryImpl implements CustomPersonRepository {
+
+    private MongoOperations mongoOperations;
+
+    @Autowired
+    public CustomPersonRepositoryImpl(MongoOperations mongoOperations){
+        this.mongoOperations = mongoOperations;
+    }
 }

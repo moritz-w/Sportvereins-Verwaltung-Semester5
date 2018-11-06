71500f4a679f66afe38a3b8c650dc8efc908b2f6
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-06 19:13:30 +0100

    Simple Test to make sure, regex validations are working added

---
+++ Sportvereinsverwaltung/controller/src/test/java/at/fhv/sportsclub/SimpleValidationTestTest.java
@@ -0,0 +1,43 @@
+package at.fhv.sportsclub;
+
+import at.fhv.sportsclub.model.person.PersonDTO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:controller-beans-test.xml"})
+public class SimpleValidationTestTest {
+
+    @Autowired
+    private Validator validator;
+
+    private PersonDTO invalidNameDTO =
+            new PersonDTO(null, "Invalid1", "Invalid2", LocalDate.now(), null, null);
+
+    public SimpleValidationTestTest() {
+    }
+
+    @Test
+    public void testRegexValidation() {
+        List<String> violationMessages = new LinkedList<>();
+        Set<ConstraintViolation<PersonDTO>> constraintViolations = this.validator.validate(this.invalidNameDTO);
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    public void setValidator(Validator validator) {
+        this.validator = validator;
+    }
+}

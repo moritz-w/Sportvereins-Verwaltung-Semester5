d4674775584f0fa04d9cdcbc2c606aa29590abb2
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-07 01:03:28 +0100

    Sample RunMe adjusted

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/RunMe.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/RunMe.java
@@ -1,22 +1,14 @@
 package at.fhv.sportsclub;
 
 import at.fhv.sportsclub.controller.DepartmentController;
-import at.fhv.sportsclub.controller.PersonController;
-import at.fhv.sportsclub.entity.person.AddressEntity;
-import at.fhv.sportsclub.entity.person.ContactEntity;
-import at.fhv.sportsclub.entity.person.PersonEntity;
 import at.fhv.sportsclub.model.dept.SportDTO;
-import at.fhv.sportsclub.model.person.PersonDTO;
-import org.dozer.DozerBeanMapper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.time.LocalDate;
 
 public class RunMe {
 
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("persist-beans.xml");
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("controller-beans.xml");
         DepartmentController controller = appContext.getBean(DepartmentController.class);
 
         for (SportDTO sportDTO : controller.getAllSports()) {

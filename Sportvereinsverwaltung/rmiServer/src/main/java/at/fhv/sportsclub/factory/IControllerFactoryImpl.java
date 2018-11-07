2fce42ae98c40314841778fccb38382ebdc2ee36
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-07 01:02:10 +0100

    RMI controller factory added

---
+++ Sportvereinsverwaltung/rmiServer/src/main/java/at/fhv/sportsclub/factory/IControllerFactoryImpl.java
@@ -0,0 +1,38 @@
+package at.fhv.sportsclub.factory;
+
+import at.fhv.sportsclub.controller.DepartmentController;
+import at.fhv.sportsclub.controller.PersonController;
+import at.fhv.sportsclub.controller.interfaces.IDepartmentController;
+import at.fhv.sportsclub.controller.interfaces.IPersonController;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.stereotype.Component;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by Alex on 06.11.2018.
+ */
+
+@Component
+public class IControllerFactoryImpl implements IControllerFactory {
+
+    @Lookup
+    public PersonController createPersonController() {
+        return null;
+    }
+    @Lookup
+    public DepartmentController createDepartmentController() {
+        return null;
+    }
+
+    @Override
+    public IPersonController getPersonController() throws RemoteException {
+        return (IPersonController) UnicastRemoteObject.exportObject(createPersonController(), 0);
+    }
+
+    @Override
+    public IDepartmentController getDepartmentController() throws RemoteException {
+        return (IDepartmentController) UnicastRemoteObject.exportObject(createDepartmentController(), 0);
+    }
+}

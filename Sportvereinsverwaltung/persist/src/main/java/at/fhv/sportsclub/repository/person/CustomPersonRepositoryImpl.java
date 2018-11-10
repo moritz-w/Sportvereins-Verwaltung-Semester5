b1fc5ede7030a1c22ab369ad40f88e49be83e4cd
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-10 02:08:27 +0100

    Optimized imports (thanks IntelliJ)

--- Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/person/CustomPersonRepositoryImpl.java
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/person/CustomPersonRepositoryImpl.java
@@ -1,11 +1,7 @@
 package at.fhv.sportsclub.repository.person;
 
-import at.fhv.sportsclub.entity.person.PersonEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository

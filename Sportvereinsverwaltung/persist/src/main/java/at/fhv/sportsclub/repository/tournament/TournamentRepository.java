ed4394cce13f77c47cdf5a5ffaa0bc4569b5f2f9
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-13 01:06:36 +0100

    Tournament repository added

--- Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/tournament/TournamentRepository.java
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/tournament/TournamentRepository.java
@@ -3,6 +3,6 @@ package at.fhv.sportsclub.repository.tournament;
 import at.fhv.sportsclub.entity.tournament.TournamentEntity;
 import at.fhv.sportsclub.repository.CommonRepository;
 
-public interface ITournamentReposetory extends CommonRepository<TournamentEntity, String>
-{
+public interface TournamentRepository extends CommonRepository<TournamentEntity, String> {
+
 }

a7eb423164610440e1b2a03bb9f181952d2cf3af
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-17 22:09:17 +0100

    Added user details to interface method  - these user details can be used to verify session ids

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SessionIdService.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SessionIdService.java
@@ -1,11 +1,13 @@
 package at.fhv.sportsclub.security.session;
 
+import at.fhv.sportsclub.model.security.UserDetails;
+
 /*
       Created: 14.11.2018
       Author: Moritz W.
       Co-Authors: 
 */
 public interface SessionIdService<T> {
-    T generateSessionId();
-    boolean validateSessionId(T sessionId);
+    T generateSessionId(UserDetails userDetails);
+    boolean validateSessionId(T sessionId, UserDetails userDetails);
 }

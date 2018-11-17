d53b4de6411a18c66c8c3ecaa75d1facc4394920
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-17 22:14:12 +0100

    Dependencies for the Authorization controller added

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/authorization/AuthorizationController.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/authorization/AuthorizationController.java
@@ -1,6 +1,9 @@
 package at.fhv.sportsclub.security.authorization;
 
 import at.fhv.sportsclub.model.security.SessionDTO;
+import at.fhv.sportsclub.security.authentication.AuthenticationController;
+import at.fhv.sportsclub.security.session.SessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /*
@@ -10,6 +13,13 @@ import org.springframework.stereotype.Component;
 */
 @Component
 public class AuthorizationController {
+
+    private final SessionManager sessionManager;
+
+    @Autowired
+    public AuthorizationController(SessionManager sessionManager){
+        this.sessionManager = sessionManager;
+    }
 
     public void checkPermissions(SessionDTO session){
 

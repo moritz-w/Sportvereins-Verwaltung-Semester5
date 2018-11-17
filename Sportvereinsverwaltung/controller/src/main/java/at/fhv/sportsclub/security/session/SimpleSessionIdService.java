21fbae552ad3faa11e6762658e0c8ee8bddba9f3
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-17 22:12:16 +0100

    Simple session ID generation added

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SimpleSessionIdService.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SimpleSessionIdService.java
@@ -1,6 +1,12 @@
 package at.fhv.sportsclub.security.session;
 
+import at.fhv.sportsclub.model.security.UserDetails;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /*
       Created: 14.11.2018
@@ -10,13 +16,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class SimpleSessionIdService implements SessionIdService<String> {
 
+    private final static Logger logger = Logger.getRootLogger();
+    private final static String secret = "9GOy5Bs7vXmmT4SFaaq9Y1WMM1fRy59Q";
+
     @Override
-    public String generateSessionId() {
-        return "000";
+    public String generateSessionId(UserDetails userDetails) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            String val = userDetails.getUserId() + userDetails.getNonce() + secret;
+            byte[] hashBytes = messageDigest.digest(val.getBytes(StandardCharsets.UTF_8));
+            return new String(hashBytes, StandardCharsets.UTF_8);
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Could not generate session ", e);
+            throw new RuntimeException("Fatal error: could not obtain encoder for given digest type");
+        }
     }
 
     @Override
-    public boolean validateSessionId(String sessionId) {
-        return true;
+    public boolean validateSessionId(String sessionId, UserDetails userDetails) {
+        return generateSessionId(userDetails).equals(sessionId);
     }
 }

3734cd4e798defefac53e33daa5c0088c7beb8a6
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-17 22:11:19 +0100

    Session manager implementation extended  - session store refactored  - session ID meta information added, that can be used to generate Session Ids  - Invalidate and refresh (not yet implemented) method added

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SessionManager.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/session/SessionManager.java
@@ -2,7 +2,11 @@ package at.fhv.sportsclub.security.session;
 
 import at.fhv.sportsclub.model.security.RoleDTO;
 import at.fhv.sportsclub.model.security.SessionDTO;
+import at.fhv.sportsclub.model.security.UserDetails;
+import at.fhv.sportsclub.security.exception.SessionInvalidException;
 
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,26 +18,68 @@ import java.util.List;
 */
 public abstract class SessionManager<T> {
 
+    private static final Long sessionTime = 360000L;
+
     private SessionIdService<T> sessionIdService;
-    private HashMap<T, List<RoleDTO>> sessionStore;
+    private HashMap<T, Session> sessionStore;
 
     public SessionManager(SessionIdService<T> sessionIdService){
         this.sessionIdService = sessionIdService;
         this.sessionStore = new HashMap<>();
     }
 
-    public SessionDTO<T> createNewSession(){
+    public SessionDTO<T> createNewSession(UserDetails userDetails){
+        if(userDetails == null){
+            // TODO message
+        }
+        userDetails.setNonce(getNonce());
+        T generatedSessionId = sessionIdService.generateSessionId(userDetails);
+
+        Long expirationTime = System.currentTimeMillis() / 1000L + sessionTime;
+        sessionStore.put(generatedSessionId, new Session(userDetails, expirationTime));
+        return new SessionDTO<>(generatedSessionId, expirationTime);
+    }
+
+    public SessionDTO<T> refreshSession(SessionDTO<T> session){
         return null;
     }
 
-    public List<RoleDTO> getSessionMeta(SessionDTO<T> session){
-        if (isExpired(session.getExpires()) || !sessionIdService.validateSessionId(session.getSessionId())){
-            return new LinkedList<>();
+    public void invalidate(SessionDTO<T> session){
+        sessionStore.remove(session.getSessionId());
+    }
+
+    public List<RoleDTO> getAccessRolesBySession(SessionDTO<T> session) throws SessionInvalidException {
+        String message;
+        if(!(message = validateSession(session)).isEmpty()){
+            throw new SessionInvalidException(message);
         }
-        return sessionStore.getOrDefault(session.getSessionId(), new LinkedList<>());
+        return sessionStore.get(session.getSessionId()).getUserDetails().getRoles();
+    }
+
+    private String validateSession(SessionDTO<T> session){
+        Session validSession = sessionStore.get(session.getSessionId());
+        if (validSession == null){
+            return "Session ID invalid";
+        }
+        UserDetails userDetails = validSession.getUserDetails();
+        if (!sessionIdService.validateSessionId(session.getSessionId(), userDetails)){
+            return "Session ID invalid";
+        }
+        if(isExpired(validSession.getExpires())){
+            sessionStore.remove(session.getSessionId());
+            return "Session expired";
+        }
+        return "";
     }
 
     private boolean isExpired(Long timestamp){
-        return false;
+        return (System.currentTimeMillis()/1000L > timestamp);
+    }
+
+    private String getNonce(){
+        SecureRandom sec = new SecureRandom();
+        byte[] values = new byte[20];
+        sec.nextBytes(values);
+        return new String(values, StandardCharsets.UTF_8);
     }
 }

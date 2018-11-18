e08de14827afc48f21b76ce0483a81d901ccc6ec
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-19 00:01:08 +0100

    DTOs for Roles and Privileges added

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/person/PersonDTO.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/person/PersonDTO.java
@@ -3,6 +3,7 @@ package at.fhv.sportsclub.model.person;
 import at.fhv.sportsclub.model.common.IDTO;
 import at.fhv.sportsclub.model.common.ResponseMessageDTO;
 import at.fhv.sportsclub.model.dept.SportDTO;
+import at.fhv.sportsclub.model.security.RoleDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -17,7 +18,7 @@ public @Data class PersonDTO implements Serializable, IDTO {
 
     public PersonDTO() {}
 
-    private static final long serialVersionUID = 5529685098267757690L;
+    private static final long serialVersionUID = 5529685098267757695L; // changed 18.11.2018
 
     private String id;
     @Pattern(regexp = "^[a-zA-ZäöüÄÖÜ\\s]*$", message = "Im Vornamen dürfen keine Zahlen oder Sonderzeichen vorkommen.")
@@ -31,6 +32,7 @@ public @Data class PersonDTO implements Serializable, IDTO {
     @NotNull
     private ContactDTO contact;
     private List<SportDTO> sports;
+    private List<RoleDTO> roles;
 
     private ResponseMessageDTO response = null;
 }
---
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/security/PrivilegeDTO.java
@@ -0,0 +1,17 @@
+package at.fhv.sportsclub.model.security;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+/*
+      Created: 18.11.2018
+      Author: Moritz W.
+      Co-Authors: 
+*/
+public @Data class PrivilegeDTO implements Serializable {
+    private String domain;
+    private Set<AccessLevel> accessLevels;
+}

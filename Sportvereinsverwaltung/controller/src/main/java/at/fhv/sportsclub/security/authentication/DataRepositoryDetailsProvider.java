2d7eb4cdd96ce99fb63aab307285be98864c6c5b
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-19 00:06:42 +0100

    UserDetails provider implementation added  - roles are fetched from DB and mapped to DTOs

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/authentication/DataRepositoryDetailsProvider.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/security/authentication/DataRepositoryDetailsProvider.java
@@ -1,9 +1,18 @@
 package at.fhv.sportsclub.security.authentication;
 
+import at.fhv.sportsclub.entity.person.PersonEntity;
+import at.fhv.sportsclub.entity.person.RoleEntity;
+import at.fhv.sportsclub.model.security.RoleDTO;
 import at.fhv.sportsclub.model.security.UserDetails;
 import at.fhv.sportsclub.repository.person.PersonRepository;
+import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /*
       Created: 14.11.2018
@@ -14,14 +23,25 @@ import org.springframework.stereotype.Component;
 public class DataRepositoryDetailsProvider implements UserDetailsProvider {
 
     private final PersonRepository repository;
+    private final Mapper mapper;
 
     @Autowired
-    public DataRepositoryDetailsProvider(PersonRepository repository){
+    public DataRepositoryDetailsProvider(PersonRepository repository, @Qualifier("generalMapper") Mapper mapper){
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     @Override
     public UserDetails getUserDetails(String userId) {
-        return null;
+        Optional<PersonEntity> entity = repository.findPersonEntityByContact_EmailAddress(userId);
+        if (!entity.isPresent()){
+            return null;
+        }
+        PersonEntity personEntity = entity.get();
+        List<RoleDTO> roles = new ArrayList<>();
+        for (RoleEntity roleEntity : personEntity.getRoles()) {
+            roles.add(this.mapper.map(roleEntity, RoleDTO.class, "RoleDTOMapping"));
+        }
+        return new UserDetails(userId, null, roles);
     }
 }

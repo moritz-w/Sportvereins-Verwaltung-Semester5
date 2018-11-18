c81b2d30fa2af966b1a80dc2d9d212ef14b50074
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-19 00:04:50 +0100

    Response Message added to Session

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/security/SessionDTO.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/security/SessionDTO.java
@@ -1,9 +1,11 @@
 package at.fhv.sportsclub.model.security;
 
+import at.fhv.sportsclub.model.common.ResponseMessageDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 /*
       Created: 14.11.2018
@@ -14,4 +16,7 @@ import java.io.Serializable;
 public @Data class SessionDTO<T> implements Serializable {
     private T sessionId;
     private Long expires;
+    private List<RoleDTO> roles;
+
+    private ResponseMessageDTO responseMessage;
 }

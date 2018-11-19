2def2b2194a257eff059a97bdd5727c03cee163a
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-20 00:41:32 +0100

    Mongo Query for Leagues by ID added

--- Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/dept/CustomDepartmentRepositoryImpl.java
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/dept/CustomDepartmentRepositoryImpl.java
@@ -5,9 +5,15 @@ import at.fhv.sportsclub.entity.dept.LeagueEntity;
 import at.fhv.sportsclub.entity.dept.SportEntity;
 import at.fhv.sportsclub.exception.DataAccessException;
 import at.fhv.sportsclub.exception.InvalidInputDataException;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.*;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Field;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.expression.spel.SpelEvaluationException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -18,6 +24,10 @@ import java.util.List;
 
 // Criteria static import to improve readability
 import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;          // project, match
+import static org.springframework.data.mongodb.core.aggregation.ArrayOperators.Filter.*;       // filter
+import org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Eq;
+import static org.springframework.data.mongodb.core.aggregation.VariableOperators.mapItemsOf;    // map
 
 /*
       Created: 12.11.2018
@@ -70,7 +80,7 @@ public class CustomDepartmentRepositoryImpl implements CustomDepartmentRepositor
 
         List<DepartmentEntity> departmentEntities = mongoOperations.find(q, DepartmentEntity.class);
         if(departmentEntities.isEmpty()){
-            throw new DataAccessException("No results could be obtained for the given id '" + id +"'");
+            throw new DataAccessException("No sports could be obtained for the given id '" + id +"'");
         }
 
         StandardEvaluationContext context = new StandardEvaluationContext(departmentEntities);
@@ -86,7 +96,7 @@ public class CustomDepartmentRepositoryImpl implements CustomDepartmentRepositor
     }
 
     //region Mongo Query
-    /*
+    /* Aggregate Query: db.Collection.aggregate([])
      * Mongo query for resolving leagues by id with different filters:
      * [
      *  {
@@ -121,7 +131,62 @@ public class CustomDepartmentRepositoryImpl implements CustomDepartmentRepositor
      * @return
      */
     @Override
-    public LeagueEntity getLeagueById(String id) {
-        return null;
+    public LeagueEntity getLeagueById(String id) throws InvalidInputDataException, DataAccessException {
+        ObjectId objectId;
+        try {
+            objectId = new ObjectId(id);
+        } catch (IllegalArgumentException e){
+            throw new InvalidInputDataException("Invalid ID type for given string '" + id + "', a hex string is required");
+        }
+        Aggregation aggregation = newAggregation(
+                match(where("sports.leagues._id").is(objectId)),        // find departments of matching league ID
+                new ProjectionOperation(
+                        Fields.fields("sports")                         // only project field sports of the department
+                ).and(
+                        aggregationOperationContext -> new Document(
+                                "$filter", new Document(                // filter only sports, where size of matching leagues > 0
+                                // for all sports apply the given filter on leagues
+                                        "input", mapItemsOf("sports").as("sport").andApply(
+                                                 innerOperationContext -> new Document(
+                                                    "leagues", new Document(
+                                                        // filter from league array, only those Documents matching the condition
+                                                        "$filter", new Document(
+                                                            "input", "$$sport.leagues"
+                                                        ).append(
+                                                            "as", "league"
+                                                        ).append(
+                                                            "cond", Eq.valueOf("$$league._id").equalToValue(
+                                                                    objectId
+                                                            ).toDocument(innerOperationContext)
+                                                        )
+                                                    )
+                                                 )
+                                            ).toDocument(aggregationOperationContext)
+                                ).append(
+                                        "as", "sport"
+                                ).append(
+                                        "cond", ComparisonOperators.Gt.valueOf(
+                                            ArrayOperators.Size.lengthOfArray("$$sport.leagues")
+                                        ).greaterThanValue(0).toDocument(aggregationOperationContext)
+                                )
+                        )
+                ).as("sports")
+        );
+        AggregationResults<DepartmentEntity> results =
+                mongoOperations.aggregate(aggregation, "Department", DepartmentEntity.class);
+        List<DepartmentEntity> entities = results.getMappedResults();
+        if(entities.isEmpty()){
+            throw new DataAccessException("No leagues could be obtained for the given id '" + id +"'");
+        }
+        StandardEvaluationContext context = new StandardEvaluationContext(entities);
+        LeagueEntity leagueEntity;
+        try {
+            leagueEntity = parser.parseExpression(
+                    "#this[0].sports[0].leagues[0]").getValue(context, LeagueEntity.class
+            );
+        } catch (SpelEvaluationException e){
+            throw new DataAccessException("Failed to extract LeagueEntity from root object");
+        }
+        return leagueEntity;
     }
 }

82f3319cd13f026df9fa25d8f722d13ad7362a8b
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-20 00:41:16 +0100

    League resolver implementation added for ID resolver

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/resolver/LeagueResolver.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/resolver/LeagueResolver.java
@@ -1,11 +1,15 @@
 package at.fhv.sportsclub.controller.resolver;
 
+import at.fhv.sportsclub.entity.dept.LeagueEntity;
+import at.fhv.sportsclub.exception.DataAccessException;
+import at.fhv.sportsclub.exception.InvalidInputDataException;
 import at.fhv.sportsclub.model.dept.LeagueDTO;
 import at.fhv.sportsclub.repository.dept.DepartmentRepository;
 import org.apache.log4j.Logger;
 import org.bson.types.ObjectId;
 import org.dozer.CustomConverter;
 import org.dozer.Mapper;
+import org.dozer.MappingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -31,12 +35,29 @@ public class LeagueResolver implements CustomConverter {
     @Override
     public Object convert(Object dest, Object source, Class<?> aClass, Class<?> aClass1) {
         if(source instanceof ObjectId){
-            // resolve from database over repo
+            return resolveFromObjectId((ObjectId) source);
         }
         else if (source instanceof LeagueDTO){
             return resolveFromDTO((LeagueDTO) source);
         }
         return null;
+    }
+
+    private LeagueDTO resolveFromObjectId(ObjectId objectId){
+        LeagueDTO leagueDTO= null;
+        try {
+            LeagueEntity sportEntity = departmentRepository.getLeagueById(objectId.toHexString());
+            try {
+                leagueDTO = resolverMapper.map(sportEntity, LeagueDTO.class, "SportEntityMappingLight");
+            } catch (MappingException e){
+                logger.fatal("Mapping from entity to domain failed", e);
+            }
+        } catch (InvalidInputDataException e) {
+            logger.error("Invalid ID input given: ", e);
+        } catch (DataAccessException e) {
+            logger.warn("Not data could be obtained for the given ID " + objectId.toHexString(), e);
+        }
+        return leagueDTO == null ? new LeagueDTO() : leagueDTO;
     }
 
     private ObjectId resolveFromDTO(LeagueDTO dto){

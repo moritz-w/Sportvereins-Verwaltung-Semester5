8de01e5ae70de8da2c8027e130cfc5afbf5dfee3
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-20 02:33:29 +0100

    Tournament Controller prototypes added

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/impl/TournamentController.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/impl/TournamentController.java
@@ -4,8 +4,11 @@ import at.fhv.sportsclub.controller.common.CommonController;
 import at.fhv.sportsclub.controller.interfaces.ITournamentController;
 import at.fhv.sportsclub.entity.team.TeamEntity;
 import at.fhv.sportsclub.entity.tournament.TournamentEntity;
+import at.fhv.sportsclub.model.common.ListWrapper;
 import at.fhv.sportsclub.model.common.ResponseMessageDTO;
 import at.fhv.sportsclub.model.team.TeamDTO;
+import at.fhv.sportsclub.model.tournament.EncounterDTO;
+import at.fhv.sportsclub.model.tournament.ParticipantDTO;
 import at.fhv.sportsclub.model.tournament.TournamentDTO;
 import at.fhv.sportsclub.repository.tournament.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Service;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @Scope("prototype")
@@ -33,23 +38,37 @@ public class TournamentController extends CommonController<TournamentDTO, Tourna
     }
 
     @Override
-    public ResponseMessageDTO saveOrUpdateEntry(TournamentDTO tournamentDTO) throws RemoteException {
-        return this.saveOrUpdate(tournamentDTO);
+    public ResponseMessageDTO addEncountersToTournament(String tournamentId, List<EncounterDTO> encounters){
+        /*
+         *  add encounters to given tournament ID
+         */
+        return null;
     }
 
+    /**
+     * For subsequently adding teams to a tournament
+     * @param tournamentDTO
+     * @param teamId
+     * @return
+     * @throws RemoteException
+     */
     @Override
-    public ResponseMessageDTO addTeamToTurnament(TournamentDTO tournamentDTO, TeamDTO teamDTO) throws RemoteException {
-
-
-
-        TournamentEntity tournament = this.map(tournamentDTO, TournamentEntity.class, "");
-        TeamEntity team = this.map(teamDTO, TeamEntity.class, "TeamDTOMappingFull");
-
-        this.tournamentReposetory.addTeamToTournament(tournament, team);
-        return new ResponseMessageDTO(/*Iwie muss das ding initialisiert werden*/);
+    public ListWrapper<ParticipantDTO> addTeamsToTournament(TournamentDTO tournamentDTO, List<String> teamId) throws RemoteException {
+        tournamentDTO.setEncounters(new LinkedList<>());        // explicitly remove encounters
+        tournamentDTO.setTeams(new LinkedList<>());             // "
+        // denormalize tournament data
+        /*
+            replaces the saveOrUpdate method for the client, tournament dto only needs flat properties (no teams no encounters)
+            iterate over list of teamIds and resolve teams from TeamController with light mapping (full false)
+            Denormalize data from team to ParticipantDTO and push data to array of tournament ID
+            Return participantDTOs that where just created (including Object Ids)
+         */
+        this.saveOrUpdate(tournamentDTO);
+        return null;
     }
 
-
-
+    public ResponseMessageDTO removeEncounter(String tournamentId, String encounterId){
+        return null;
+    }
 
 }
--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/interfaces/ITournamentController.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/controller/interfaces/ITournamentController.java
@@ -1,19 +1,18 @@
 package at.fhv.sportsclub.controller.interfaces;
 
+import at.fhv.sportsclub.model.common.ListWrapper;
 import at.fhv.sportsclub.model.common.ResponseMessageDTO;
-import at.fhv.sportsclub.model.team.TeamDTO;
+import at.fhv.sportsclub.model.tournament.EncounterDTO;
+import at.fhv.sportsclub.model.tournament.ParticipantDTO;
 import at.fhv.sportsclub.model.tournament.TournamentDTO;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ITournamentController extends Remote {
-
-
     ArrayList<TournamentDTO> getAllEntries() throws RemoteException;
-    ResponseMessageDTO saveOrUpdateEntry(TournamentDTO tournamentDTO) throws RemoteException;
-
-    ResponseMessageDTO addTeamToTurnament(TournamentDTO tournament, TeamDTO team) throws RemoteException;
-
+    ListWrapper<ParticipantDTO> addTeamsToTournament(TournamentDTO tournamentDTO, List<String> teamId) throws RemoteException;
+    ResponseMessageDTO addEncountersToTournament(String tournamentId, List<EncounterDTO> encounters);
 }

27afbeabfae0d1ba1bb3bb9bc782f4f51d242f75
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-11-21 02:47:54 +0100

    Prototype methods added for tournament udpate queries

--- Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/tournament/CustomTournamentRepository.java
+++ Sportvereinsverwaltung/persist/src/main/java/at/fhv/sportsclub/repository/tournament/CustomTournamentRepository.java
@@ -1,11 +1,14 @@
 package at.fhv.sportsclub.repository.tournament;
 
 import at.fhv.sportsclub.entity.team.TeamEntity;
+import at.fhv.sportsclub.entity.tournament.EncounterEntity;
+import at.fhv.sportsclub.entity.tournament.ParticipantEntity;
 import at.fhv.sportsclub.entity.tournament.TournamentEntity;
 
+import java.util.List;
+
 public interface CustomTournamentRepository {
-
-
-    void addTeamToTournament(TournamentEntity tournament, TeamEntity team);
-
+    void addTeamToTournament(String tournamentId, List<ParticipantEntity> participantEntities);
+    void addEncounterToTournament(String tournamentId, List<EncounterEntity> encounters);
+    void addPersonsToParticipant(String participantId, List<String> participants);
 }

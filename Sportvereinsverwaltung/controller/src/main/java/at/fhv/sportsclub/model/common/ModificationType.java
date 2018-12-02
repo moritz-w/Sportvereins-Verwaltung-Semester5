4cd1dd0bcd90d07bef97c610cd58fc69b5e9c612
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-12-03 00:20:42 +0100

    Merged UPDATED and ADDED to MODIFIED

--- Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/common/ModificationType.java
+++ Sportvereinsverwaltung/controller/src/main/java/at/fhv/sportsclub/model/common/ModificationType.java
@@ -6,5 +6,5 @@ package at.fhv.sportsclub.model.common;
       Co-Authors: 
 */
 public enum ModificationType {
-    ADDED, REMOVED, UPDATED, NONE
+    MODIFIED, REMOVED, NONE         // MODIFIED wraps updated and added in one
 }

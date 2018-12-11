2f8030bb68cbeb643c8d153e551f495e5e9bdcf9
drunkenMonkey-1 <32767920+drunkenMonkey-1@users.noreply.github.com>
2018-12-11 19:30:01 +0100

    Added EJB Remote interfaces

---
+++ Sportvereinsverwaltung/enterpriseBeans/src/main/java/at/fhv/sportsclub/ejb/interfaces/ITeamController.java
@@ -0,0 +1,26 @@
+package at.fhv.sportsclub.ejb.interfaces;
+
+import at.fhv.sportsclub.model.common.ListWrapper;
+import at.fhv.sportsclub.model.common.ResponseMessageDTO;
+import at.fhv.sportsclub.model.security.SessionDTO;
+import at.fhv.sportsclub.model.team.TeamDTO;
+
+import javax.ejb.Remote;
+import java.util.ArrayList;
+
+/**
+ * businessMonkey
+ * at.fhv.sportsclub.controller
+ * ITeamController
+ * 07.11.2018 sge
+ */
+@Remote
+public interface ITeamController  {
+    ArrayList<TeamDTO> getAllEntries(SessionDTO session) ;
+    ResponseMessageDTO saveOrUpdateEntry(SessionDTO session, TeamDTO teamDTO) ;
+    TeamDTO getById(SessionDTO session, String id) ;
+    TeamDTO getEntryDetails(SessionDTO session, String id) ;
+    ListWrapper<TeamDTO> getByLeague(SessionDTO session, String leagueId) ;
+    ListWrapper<TeamDTO> getBySport(SessionDTO session, String sportId) ;
+    ListWrapper<TeamDTO> getTeamsByTrainerId(SessionDTO session, String trainerPersonId) ;
+}
---
+++ Sportvereinsverwaltung/enterpriseBeans/src/main/java/at/fhv/sportsclub/ejb/interfaces/ITournamentController.java
@@ -0,0 +1,17 @@
+package at.fhv.sportsclub.ejb.interfaces;
+
+import at.fhv.sportsclub.model.common.ListWrapper;
+import at.fhv.sportsclub.model.security.SessionDTO;
+import at.fhv.sportsclub.model.tournament.TournamentDTO;
+
+import javax.ejb.Remote;
+
+
+@Remote
+public interface ITournamentController {
+    ListWrapper<TournamentDTO> getAllEntries(SessionDTO session) ;
+    TournamentDTO getEntryDetails(SessionDTO session, String id) ;
+    TournamentDTO getById(SessionDTO session, String id) ;
+    ListWrapper<TournamentDTO> getTournamentByTrainerId(SessionDTO session, String personId) ;
+    TournamentDTO saveOrUpdateEntry(SessionDTO session, TournamentDTO tournament) ;
+}
